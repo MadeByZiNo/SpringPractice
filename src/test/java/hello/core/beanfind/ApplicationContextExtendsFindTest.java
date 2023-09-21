6583360239444e5fc58cef6cc43edd3935e529ba
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-09-21 10:21:38 +0900

    스프링 빈 조회 - 상속관계

---
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,67 @@
+package hello.core.beanfind;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class ApplicationContextExtendsFindTest {
+    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(TestConfig.class);
+
+
+    @Test
+    @DisplayName("부모타입으로 조회 시, 자식이 둘 이상 있으면 중복오류 발생")
+    void findBeanByParentTypeDuplicate() {
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> applicationContext.getBean(DiscountPolicy.class));
+    }
+
+    @Test
+    @DisplayName("부모타입으로 조회 시, 빈 이름으로 지정하면 된다.")
+    void findBeanByParentTypeBeanName() {
+        DiscountPolicy rateDiscountPolicy = applicationContext.getBean("rateDiscountPolicy",DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모타입으로 모두 조회")
+    void findAllBeanByParentType() {
+        Map<String, DiscountPolicy> beansOfType = applicationContext.getBeansOfType(DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key + = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("Object타입으로 모두 조회")
+    void findAllBeanByObjectType() {
+        Map<String, Object> beansOfType = applicationContext.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key + = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+        @Bean
+        public DiscountPolicy fixDiscountPolicy() {
+            return new FixDiscountPolicy();
+        }
+    }
+}
